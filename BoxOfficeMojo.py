60e97c954910f5f8e9d085e2bc5510f4d1aeae43
MartinSkytte <m.s.s@keepahead.dk>
2014-11-24 12:53:03 +0100

    Updated BoxOfficeMojo.py

--- BoxOfficeMojo.py
+++ BoxOfficeMojo.py
@@ -1,23 +1,79 @@
 __author__ = 'Martin Skytte'
 
-from re import findall
+from re import findall, finditer
 from urllib import urlopen, quote
+from itertools import islice
 from pprint import pprint
 
 
 class BoxOfficeMojo():
+    """
+    Class for getting earnings information about a movie from BoxOfficeMojo.com
+    """
 
     def __init__(self):
-        self.base_url = "http://www.boxofficemojo.com/"
+        """
+        Initialize base information for the class
+        """
+        self.base_url = "http://www.BoxOfficeMojo.com/"
 
     def _find_search_results(self, page):
+        """
+        Find a specific html pattern and return a list of BoxOfficeMojo page id names and readable names,
+        from a given html page.
+
+        :param page:
+        :return list of dicts:
+        """
         pattern = r'<a href="/movies/\?id\=([A-Za-z0-9\_\-]*?)\.htm">([A-Za-z0-9\_\- \&\:\?\.\(\)\'&amp;\!]*?)</a>'
         return [{link[1], link[0]} for link in findall(pattern, page)]
 
+    def _find_earnings(self, page):
+        """
+        Find a specific html pattern and return a list of movie earnings, from a given html page.
+
+        :param page:
+        :return list of ints:
+        """
+        pattern = r'<font color="#000080">\$([0-9,]*?)</font>'
+        return [int(earning.replace(",", "")) for earning in findall(pattern, page)]
+
+    def _find_first_seven_earnings(self, page):
+        pattern = r'<font color="#000080">\$([0-9,]*?)</font>'
+        return [int(earning.replace(",", "")) for earning in islice(finditer(pattern, page, flags=0), 7)]
+
     def get_search_results(self, search_word):
+        """
+        Search for a movie using BoxOfficeMojos search engine. When given a word it will search the BoxOfficeMojo site
+        
+        :param search_word:
+        :return list of dict:
+        """
         url = self.base_url + "search/?q=" + quote(search_word)
         page = urlopen(url)
         return self._find_search_results(page.read())
+
+    def get_movie_earnings(self, box_office_mojo_name):
+        """
+        Get all daily earnings from BoxOfficeMojo
+
+        :param box_office_mojo_name: 
+        :return list of ints:
+        """
+        url = self.base_url + "movies/?page=daily&id=" + box_office_mojo_name + ".htm"
+        page = urlopen(url)
+        return self._find_ernings(page)
+
+    def get_first_seven_days(self, box_office_mojo_name):
+        """
+        Get first 7 days of earnings if available from BoxOfficeMojo
+
+        :param box_office_mojo_name:
+        :return list of ints:
+        """
+        url = self.base_url + "movies/?page=daily&id=" + box_office_mojo_name + ".htm"
+        page = urlopen(url)
+        return self._find_first_seven_earnings(page)
 
 mojo = BoxOfficeMojo()
 pprint(mojo.get_search_results("john"))
--- YoutubeTest.py
+++ YoutubeTest.py
@@ -5,26 +5,29 @@ from xml.dom import minidom
 
 
 
-def ListComments(jsonObject):
+def ListComments(jsonObject, count):
     for item in jsonObject["feed"]["entry"]:
+        count += 1
+        print "count: " + repr(count)
         print item ["content"]["$t"]
         print "======================================"
 
     for item in jsonObject["feed"]["link"]:
         if (item["rel"] == "next"):
-            GetNextPage(item["href"])
+            GetNextPage(item["href"], count)
 
-def GetNextPage(url):
+def GetNextPage(url, count):
     comments = urlopen(url)
-    ListComments(json.load(comments))
+    ListComments(json.load(comments), count)
 
 
 def GetComments(video_id):
     url = "https://gdata.youtube.com/feeds/api/videos/" + video_id + "/comments?alt=json&start-index=1&max-results=50&prettyprint=true&orderby=published"
     #url = "http://gdata.youtube.com/feeds/api/videos/" + video_id + "/comments?prettyprint=true"
     #url = "http://gdata.youtube.com/feeds/api/videos/" + video_id + "/comments?v=2&alt=json&start-index=1"
+    count = 0
     comments = urlopen(url)
-    ListComments(json.load(comments))
+    ListComments(json.load(comments), count)
 
 
 GetComments("FN3YaybNJ2s") #("n_y2rayU_y8")
