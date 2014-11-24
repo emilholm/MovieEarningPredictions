7e9e2c7fe6d503903675b288bde0018910a000e8
emilholm <emilholm@gmail.com>
2014-11-24 17:08:38 +0100

    Positive/negative sentiment

--- YoutubeTest.py
+++ YoutubeTest.py
@@ -1,18 +1,37 @@
 __author__ = 'emill05'
 from urllib2 import urlopen
+import nltk
 import json
 from xml.dom import minidom
+from YouTubeComments import YouTubeComments as yt
+import gdata.youtube.service
+
+# We will require read-only access to the YouTube Data and Analytics API.
+YOUTUBE_SCOPES = ["https://www.googleapis.com/auth/youtube.readonly",
+  "https://www.googleapis.com/auth/yt-analytics.readonly"]
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+YOUTUBE_ANALYTICS_API_SERVICE_NAME = "youtubeAnalytics"
+YOUTUBE_ANALYTICS_API_VERSION = "v1"
+
+# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = ""
 
 
 
 def ListComments(jsonObject):
+    commentString = ""
     for item in jsonObject["feed"]["entry"]:
         print item ["content"]["$t"]
-        print "======================================"
+        #print "======================================"
+        commentString = commentString + item ["content"]["$t"]
 
     for item in jsonObject["feed"]["link"]:
         if (item["rel"] == "next"):
             GetNextPage(item["href"])
+
+
+    #Analyze_comments(commentString)
 
 def GetNextPage(url):
     comments = urlopen(url)
@@ -27,4 +46,15 @@ def GetComments(video_id):
     ListComments(json.load(comments))
 
 
-GetComments("FN3YaybNJ2s") #("n_y2rayU_y8")
+def Analyze_comments(comments):
+    youtube = yt()
+    print youtube.get_all_comments("FN3YaybNJ2s")
+
+
+GetComments("2AUmvWm5ZDQ")
+#for item in youtube.get_all_comments("FN3YaybNJ2s"):
+#    test = test + item
+#nltk.word_tokenize(test)
+
+#ytfeed = yts.GetYouTubeVideoCommentFeed(video_id="pXhcPJK5cMc")
+
--- classifier.py
+++ classifier.py
@@ -1,62 +1,72 @@
+# -*- coding: utf-8 -*-
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
 import nltk.tokenize
 import re
- 
-def word_feats(words):
-    return dict([(word, True) for word in words])
- 
-negids = movie_reviews.fileids('neg')
-posids = movie_reviews.fileids('pos')
- 
-negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
-posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
 
-print negfeats[0]
-
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
- 
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
- 
-classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-classifier.show_most_informative_features()
-
-f = open('johnwickcomments.txt', 'r')
-comments = '';
-for item in f:
-    comments = comments + item
+class Classifier():
+    def __init__(self):
+        self.train_classifier()
 
 
-words = comments.split() #use regex to clean text
-stemmedwords = ['']
+    def word_feats(self,words):
+        return dict([(word, True) for word in words])
 
-porter = nltk.PorterStemmer() #use stemmer to normalize
+    def train_classifier(self):
+        negids = movie_reviews.fileids('neg')
+        posids = movie_reviews.fileids('pos')
 
-for word in words:
-    try:
-        print porter.stem(word)
-    except StandardError:
-        print('')
+        negfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+        posfeats = [(self.word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
 
-classifier.prob_classify(word_feats(['bullshit','bad','crap','shit','fuck'])).__dict__
-#{'_prob_dict': {'neg': -0.5845758742331277, 'pos': -1.5857360646711278}, '_log': True} neg/pos=0,36
+        negcutoff = len(negfeats)*3/4
+        poscutoff = len(posfeats)*3/4
 
-classifier.prob_classify(word_feats(['outstanding','amazing','good','great'])).__dict__
-#{'_prob_dict': {'neg': -5.192535556923387, 'pos': -0.040001146956223366}, '_log': True} neg/pos=130
+        trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+        testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
 
-negScore = classifier.prob_classify(word_feats(words)).__dict__.values()[0]['neg']
-#{'_prob_dict': {'neg': -67.37839930360587, 'pos': 0.0}, '_log': True}
-posScore = classifier.prob_classify(word_feats(words)).__dict__.values()[0]['pos']
+        self.classifier = NaiveBayesClassifier.train(trainfeats)
 
-if negScore < posScore : print 'The text is POSITIVE'
-else : print 'The text is NEGATIVE'
 
-classifier.prob_classify(word_feats(words))
+    def classify_text(self,text):
+        porter = nltk.PorterStemmer() #use stemmer to normalize
 
-#classifier.classify(words)
-#words.classify()
+        pattern = r"""(?ux)
+        (?:[^\W\d_]\.)+
+        | [^\W\d_]+(?:-[^\W\d_])*(?:’s)?
+        | \d{4}
+        | \d{1,3}(?:,\d{3})*
+        | \$\d+(?:\.\d{2})?
+        | \d{1,3}(?:\.\d+)?\s%
+        | \.\.\.
+        | [.,;"’?!():-_‘/]
+        """
+
+        words = re.findall(pattern, text)
+        stemmedWords = ''
+
+        for word in words:
+            try:
+                stemmedWords = stemmedWords + ' ' + porter.stem(word)
+                #words.append(porter.stem(word))
+            except:
+                continue
+
+        print stemmedWords
+
+        tokenizedWords = nltk.tokenize.sent_tokenize(stemmedWords)
+
+        negScore = self.classifier.prob_classify(self.word_feats(tokenizedWords)).prob('neg') #classifier
+        posScore = self.classifier.prob_classify(self.word_feats(tokenizedWords)).prob('pos')
+        return {'neg': negScore, 'pos': posScore}
+
+
+def main():
+    c = Classifier()
+    score = c.classify_text(open('johnwickcomments.txt', 'r').read())
+    print score['neg']
+    print score['pos']
+
+if __name__ == '__main__':
+    main()
