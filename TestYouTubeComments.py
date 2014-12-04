6097a09531b8ce035ff37eb11551d22b399188dc
emilholm <emilholm@gmail.com>
2014-12-04 19:58:17 +0100

    Deleted unused files

--- TestYouTubeComments.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'martin'
-
-from YouTubeComments import YouTubeComments as yt
-
-youtube = yt()
-
-print youtube.get_all_comments("FN3YaybNJ2s")
-print len(youtube.get_all_comments_from_last_call)
